c2d4c5952617a162468b2b1ff09e86d76cd4086e
iafominykh <iafominykh@yandex.ru>
2023-07-17 19:09:14 +0300

    hw20.1 task2&3

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=150, verbose_name='Название')
+    specification = models.TextField(verbose_name='Описание')
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=250, verbose_name='Наименование')
+    specification = models.TextField(verbose_name='Описание')
+    preview_image = models.ImageField(upload_to='product/', verbose_name='Изображение')
+    category = models.CharField(max_length=150, verbose_name='Категория')
+    price = models.IntegerField(verbose_name='Цена')
+    creation_date = models.DateField(auto_now_add=True, verbose_name='Дата создания')
+    last_modified = models.DateField(auto_now=True, verbose_name='Дата последнего изменения')
+
+    def __str__(self):
+        return f'{self.title}'
+
+    class Meta:
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
